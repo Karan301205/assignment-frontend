fdb2f1ff7ac4913503130eb0455d6ebf5336d60b
Karan301205 <karan301205@gmail.com>
2026-04-05 18:47:24 +0530

    chore: update base URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5001/api' });
+const API = axios.create({
+  baseURL: 'https://assignment-backend-byxf.onrender.com/api'
+});
 
-API.interceptors.request.use((req) => {
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    req.headers['x-auth-token'] = token;
+    config.headers.Authorization = `Bearer ${token}`;
   }
-  return req;
+  return config;
 });
 
 export default API;
